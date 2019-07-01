51787a240029e557b686b9dfc5821988c2adca36
dani-interrante-phl <dani.interrante@phila.gov>
2019-07-01 10:39:34 -0400

    Cleaned Additional Bugs

---
+++ BatchFiles/PR_FLAGS.bat
@@ -0,0 +1,17 @@
+E:\ArcGISPro\bin\Python\envs\nightly_etl\python.exe E:\LI_PLAN_REVIEW_FLAGS\PyFiles\PR_FLAGS_TABLE.py
+if ERRORLEVEL 1 goto Failed
+E:\ArcGISPro\bin\Python\envs\nightly_etl\python.exe E:\LI_PLAN_REVIEW_FLAGS\PyFiles\ParcelBaseZoning.py
+if ERRORLEVEL 1 goto Failed
+E:\ArcGISPro\bin\Python\envs\nightly_etl\python.exe E:\LI_PLAN_REVIEW_FLAGS\PyFiles\ParcelOverlayZoning.py
+if ERRORLEVEL 1 goto Failed
+E:\ArcGISPro\bin\Python\envs\nightly_etl\python.exe E:\LI_PLAN_REVIEW_FLAGS\PyFiles\ParcelZoningRCO.py
+if ERRORLEVEL 1 goto Failed
+E:\ArcGISPro\bin\Python\envs\nightly_etl\python.exe E:\LI_PLAN_REVIEW_FLAGS\PyFiles\CopyToEnterprise.py
+if ERRORLEVEL 1 goto Failed
+
+goto Success
+
+:Failed
+set EXITCODE=1
+:Success
+EXIT /B %EXITCODE%
